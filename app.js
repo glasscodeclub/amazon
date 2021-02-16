5fd463c0ab4a2dd97c798ee20ddaff8af2106706
glasscode-arnav <arnav.glasscode@gmail.com>
2021-02-16 17:03:39 +0530

    dashboard products+ 16feb

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var express                 = require("express"),
     LocalStrategy           = require("passport-local"),
     passportLocalMongoose   = require("passport-local-mongoose"),
     homeroutes              = require("./routes/home.routes"),
-    authroutes              = require("./routes/auth.routes")
+    authroutes              = require("./routes/auth.routes"),
+    dashboardroute          = require("./routes/dashboard.routes")
     
 var app = express();
 var port=4000;
@@ -25,10 +26,7 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    next();
-});
+
 
 app.set('view engine','ejs');
 //
@@ -38,12 +36,17 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 app.get("/", function(req, res){
     res.redirect("/auth/signup");
 })
 app.use("/home", homeroutes);
 app.use("/auth", authroutes);
+app.use("/dashboard", dashboardroute);
 
 app.listen(port, function(){
     console.log("connected to port : ",port);
