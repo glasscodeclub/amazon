888074c365aec9511e996f66f854825038bfdc5c
glasscode-arnav <arnav.glasscode@gmail.com>
2021-03-12 13:53:07 +0530

    wishlist + Mar 12

--- routes/wishlist.routes.js
+++ routes/wishlist.routes.js
@@ -4,12 +4,30 @@ var wishlistLib = require("../lib/wishlist.lib")
 var WishList = require("../models/wishlist")
 var middlewares = require("../middlewares/auth");
 
+router.get("/", middlewares.isLoggedIn, function(req,res){
+    let filter={owner: req.user._id };
+    wishlistLib.findOne(filter, function(err, list) {
+        if(err)
+            console.log(err);
+        if(list != null){
+            res.render("./pages/wishlist", {products: list.items});
+        }
+        else
+            res.render("./pages/wishlist", {products: {}});
+    });
+});
+
+router.get("/added", middlewares.isLoggedIn, function(req,res){
+    res.redirect("/wishlist");
+});
+
 router.post("/add/:id",middlewares.isLoggedIn, function(req,res){
     var item = req.body.product_id,
         price = parseInt(req.body.price),
         image = req.body.image;
         itemname = req.body.name;
-    var listitem = {item, price, image, itemname};
+        discount = req.body.discount;
+    var listitem = {item, price, image, itemname, discount};
     var owner = req.user._id;
     var items = [listitem];
     var userlist = {owner, items};
@@ -32,25 +50,29 @@ router.post("/add/:id",middlewares.isLoggedIn, function(req,res){
             }
             if(finditem == false){
                 list.items.push(listitem);
-
             }
             list.save();
         }
     });
-    res.redirect("/wishlist");
+    res.redirect("/wishlist/added");
 });
 
-router.get("/", middlewares.isLoggedIn, function(req,res){
-    let filter={owner: req.user._id };
+router.post("/remove/:user_id/:item_id",middlewares.isLoggedIn,function(req,res){
+    var item = req.params.item_id;
+    let filter={owner: req.params.user_id };
     wishlistLib.findOne(filter, function(err, list) {
         if(err)
             console.log(err);
-        if(list != null){
-            res.render("./pages/wishlist", {products: list.items});
+
+        for(var i=0;i<list.items.length;i++){
+            if(list.items[i].item == item){
+                list.items.splice(i,1);
+                break;
+            }
         }
-        else
-            res.render("./pages/wishlist", {products: {}});
-    });
-});
+        list.save();
+        res.redirect("/wishlist");
+    })
+})
 
 module.exports = router;
